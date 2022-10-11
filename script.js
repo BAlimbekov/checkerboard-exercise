125314565a95b583ae431e2755e82e89ea60382a
BAlimbekov <alimbekovbekmambet@gmail.com>
2022-10-10 23:22:48 -0500

    checkeboard game has not finished

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-// Your JS goes here
+var square = document.createElement('div');
+//var randomColor = Math.floor(Math.rendom()*16777215).toString(16);
+square.style.width = '11.1%';
+square.style.paddingBottom = '11.1%';
+square.style.backgroundColor = 'blue';
+square.style.float = 'left'
+
+
+let body = document.querySelector('body')
+
+
+for (let i = 0; i < 81; i++) {
+
+    let color = '#' + Math.floor(Math.random() * 16777215).toString(16)
+    square.style.backgroundColor = color;
+    // if(i%2 ===0) {
+    //     square.style.backgroundColor = 'red'
+    // } else {
+    //     square.style.backgroundColor = 'black'
+    //        }
+    body.appendChild(square.cloneNode())
+}
+
+
